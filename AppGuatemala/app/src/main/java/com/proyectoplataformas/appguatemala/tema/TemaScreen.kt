684e9d9348a74042fd67d4460edd06bd4afa02f0
fabianpradod <fabian.prado2004@gmail.com>
2024-09-27 17:29:13 -0600

    Update TemaScreen.kt

--- AppGuatemala/app/src/main/java/com/proyectoplataformas/appguatemala/tema/TemaScreen.kt
+++ AppGuatemala/app/src/main/java/com/proyectoplataformas/appguatemala/tema/TemaScreen.kt
@@ -1,40 +1,194 @@
 package com.proyectoplataformas.appguatemala.tema
 
-
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.proyectoplataformas.appguatemala.CategoryType
+import com.proyectoplataformas.appguatemala.Noticia
+import kotlinx.serialization.Serializable
 import com.proyectoplataformas.appguatemala.R
+import com.proyectoplataformas.appguatemala.getAllNoticias
+import com.proyectoplataformas.appguatemala.getAllTypeNoticias
+import com.proyectoplataformas.appguatemala.ui.theme.AppGuatemalaTheme
 
+@Preview
 @Composable
-fun TemaScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.tikal1),
-            contentDescription = "Lugares Turísticos",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().height(200.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Lugares Turísticos de Guatemala")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Explora los hermosos destinos turísticos de Guatemala, como Tikal.")
+private fun PreviewTemaScreen() {
+    AppGuatemalaTheme {
+        Surface {
+            TemaScreen(
+                type = CategoryType.Instrumentos,
+                onNoticiaBackClick = {},
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun PreviewTemaScreen() {
-    TemaScreen()
+fun TemaGrid(
+    categoryType: CategoryType,
+    onNoticiaClick: (Noticia) -> Unit
+) {
+    val isInPreview = LocalInspectionMode.current
+    val noticias = getAllTypeNoticias(categoryType)
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(noticias) { noticia ->
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = noticia.name,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(0.45f)
+                            .aspectRatio(1f)
+                    ) {
+                        if (isInPreview) {
+                            Image(
+                                painter = painterResource(id = R.drawable.tikal1),
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth(),
+                                contentScale = ContentScale.Crop
+                            )
+                        } else {
+                            Image(
+                                painter = painterResource(
+                                    id = LocalContext.current.resources.getIdentifier(
+                                        noticia.image, "drawable", LocalContext.current.packageName
+                                    )
+                                ),
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+
+                    Box(
+                        modifier = Modifier
+                            .weight(0.55f)
+                    ) {
+                        Text(
+                            text = noticia.actualText,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally // Centers content horizontally
+                ) {
+                    Text(
+                        text = "Conoce más sobre ${noticia.name}",
+                        style = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.primary),
+                        modifier = Modifier
+                            .clickable { onNoticiaClick(noticia) }
+                    )
+                }
+            }
+        }
+    }
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TemaScreen(
+    type: CategoryType,
+    onNoticiaBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (modifier = Modifier.fillMaxSize())
+    {
+        TopAppBar(
+            title = {
+                Text(text = type.name)
+            },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back",
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier
+                        .clickable { onNoticiaBackClick() }
+                        .padding(start = 16.dp)
+                )
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.onPrimary
+            ),
+        )
+        TemaGrid(
+            categoryType = type,
+            onNoticiaClick = {}
+        )
+    }
+}
+
